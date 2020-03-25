28b8b9bfd3fc30dee0f4e4a48a5ce3e11df7869f
Navix <hazydayz0@protonmail.com>
2020-03-25 19:27:09 +0100

    Updates

---
+++ precache-manifest.06dee70ef25f9c55273a6bb6cd429a2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5681d86f6522ced90cb64ab535dcb6b",
+    "url": "/reactapp-test/index.html"
+  },
+  {
+    "revision": "fd3926a0f05432e2be2c",
+    "url": "/reactapp-test/static/css/2.72d5056d.chunk.css"
+  },
+  {
+    "revision": "9b77d99a774395e1c7e8",
+    "url": "/reactapp-test/static/css/main.427d8dd7.chunk.css"
+  },
+  {
+    "revision": "fd3926a0f05432e2be2c",
+    "url": "/reactapp-test/static/js/2.5e71b3eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp-test/static/js/2.5e71b3eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b77d99a774395e1c7e8",
+    "url": "/reactapp-test/static/js/main.1c30d819.chunk.js"
+  },
+  {
+    "revision": "464c275c5840fec56148",
+    "url": "/reactapp-test/static/js/runtime-main.61866085.js"
+  }
+]);
